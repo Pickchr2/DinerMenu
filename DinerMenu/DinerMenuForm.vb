7c4374f17232bf0e453d1d9d8b208353a6f02a8c
Pickchr2 <Cpickens1270@gmail.com>
2023-03-25 17:13:08 -0600

    Add project files.

---
+++ DinerMenu/DinerMenuForm.vb
@@ -0,0 +1,12 @@
+﻿'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Diner Menu
+'https://github.com/Pickchr2/DinerMenu.git
+
+Option Explicit On
+Option Strict On
+
+Public Class DinerMenuForm
+
+End Class
