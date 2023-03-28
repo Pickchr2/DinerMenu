6853f319e2034a0e83e53095dda017bd0ff67b68
Pickchr2 <Cpickens1270@gmail.com>
2023-03-27 19:40:02 -0600

    Menu strip and button hotkeys

--- DinerMenu/DinerMenuForm.Designer.vb
+++ DinerMenu/DinerMenuForm.Designer.vb
@@ -30,13 +30,22 @@ Partial Class DinerMenuForm
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
         Me.DinerMenuToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SoupToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaladToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FishToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'DinerNameLabel
         '
         Me.DinerNameLabel.AutoSize = True
         Me.DinerNameLabel.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.DinerNameLabel.Location = New System.Drawing.Point(76, 9)
+        Me.DinerNameLabel.Location = New System.Drawing.Point(76, 33)
         Me.DinerNameLabel.Name = "DinerNameLabel"
         Me.DinerNameLabel.Size = New System.Drawing.Size(606, 47)
         Me.DinerNameLabel.TabIndex = 0
@@ -48,7 +57,7 @@ Partial Class DinerMenuForm
         Me.SoupButton.Name = "SoupButton"
         Me.SoupButton.Size = New System.Drawing.Size(136, 44)
         Me.SoupButton.TabIndex = 2
-        Me.SoupButton.Text = "Daily Soup"
+        Me.SoupButton.Text = "Daily S&oup"
         Me.DinerMenuToolTip.SetToolTip(Me.SoupButton, "Click to display The today's soup option.")
         Me.SoupButton.UseVisualStyleBackColor = True
         '
@@ -58,7 +67,7 @@ Partial Class DinerMenuForm
         Me.SaladButton.Name = "SaladButton"
         Me.SaladButton.Size = New System.Drawing.Size(136, 44)
         Me.SaladButton.TabIndex = 3
-        Me.SaladButton.Text = "Daily Salad"
+        Me.SaladButton.Text = "Daily S&alad"
         Me.DinerMenuToolTip.SetToolTip(Me.SaladButton, "Click to display The today's salad option.")
         Me.SaladButton.UseVisualStyleBackColor = True
         '
@@ -68,7 +77,7 @@ Partial Class DinerMenuForm
         Me.FishButton.Name = "FishButton"
         Me.FishButton.Size = New System.Drawing.Size(136, 44)
         Me.FishButton.TabIndex = 4
-        Me.FishButton.Text = "Daily Fish"
+        Me.FishButton.Text = "Daily &Fish"
         Me.DinerMenuToolTip.SetToolTip(Me.FishButton, "Click to display The today's fish option.")
         Me.FishButton.UseVisualStyleBackColor = True
         '
@@ -78,18 +87,71 @@ Partial Class DinerMenuForm
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(136, 44)
         Me.ExitButton.TabIndex = 5
-        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.Text = "&Exit"
         Me.DinerMenuToolTip.SetToolTip(Me.ExitButton, "Click to close the application.")
         Me.ExitButton.UseVisualStyleBackColor = True
         '
         'DisplaySpecialLabel
         '
         Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(76, 84)
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(76, 93)
         Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
-        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(606, 173)
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(606, 180)
         Me.DisplaySpecialLabel.TabIndex = 1
         Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(761, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SoupToolStripMenuItem, Me.SaladToolStripMenuItem, Me.FishToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'SoupToolStripMenuItem
+        '
+        Me.SoupToolStripMenuItem.Name = "SoupToolStripMenuItem"
+        Me.SoupToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SoupToolStripMenuItem.Text = "Soup"
+        '
+        'SaladToolStripMenuItem
+        '
+        Me.SaladToolStripMenuItem.Name = "SaladToolStripMenuItem"
+        Me.SaladToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SaladToolStripMenuItem.Text = "Salad"
+        '
+        'FishToolStripMenuItem
+        '
+        Me.FishToolStripMenuItem.Name = "FishToolStripMenuItem"
+        Me.FishToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FishToolStripMenuItem.Text = "Fish"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'DinerMenuForm
         '
@@ -105,10 +167,14 @@ Partial Class DinerMenuForm
         Me.Controls.Add(Me.SaladButton)
         Me.Controls.Add(Me.SoupButton)
         Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "DinerMenuForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Christopher's Cafe Specialty Menu"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -121,4 +187,12 @@ Partial Class DinerMenuForm
     Friend WithEvents ExitButton As Button
     Friend WithEvents DisplaySpecialLabel As Label
     Friend WithEvents DinerMenuToolTip As ToolTip
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SoupToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaladToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FishToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
 End Class
--- DinerMenu/DinerMenuForm.vb
+++ DinerMenu/DinerMenuForm.vb
@@ -8,11 +8,11 @@ Option Explicit On
 Option Strict On
 
 Public Class DinerMenuForm
-    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click, ExitToolStripMenuItem.Click
         Me.Close()
     End Sub
 
-    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click, SoupToolStripMenuItem.Click
         DisplaySpecialLabel.Text = "~Grandma's Garden-Fresh Giambotta~" _
         & vbCrLf & vbCrLf _
         & "A wonderful, Italian vegetable stew, preparred with stewed tomatoes, green beans, diced potatoes, sliced mushrooms, ground hot Italian sausage, and a spledid blend of Italian herbs and spices!" _
@@ -20,7 +20,7 @@ Public Class DinerMenuForm
         & "Served with a slice of buttery garlic ciabatta bread!"
     End Sub
 
-    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click, SaladToolStripMenuItem.Click
         DisplaySpecialLabel.Text = "~Mom's Simple Caesar Salad~" _
         & vbCrLf & vbCrLf _
         & "This really is the best homemade Caesar salad! With crunchy romaine lettuce, finely shredded parmesan cheese, and a robust, creamy homemade caesar salad dressing." _
@@ -28,11 +28,15 @@ Public Class DinerMenuForm
         & "Served with a hardy handful of garlic herb croutons!"
     End Sub
 
-    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click, FishToolStripMenuItem.Click
         DisplaySpecialLabel.Text = "~Dad's Secret Smoked Salmon~" _
         & vbCrLf & vbCrLf _
         & "This smoked salmon has an unmatched depth of flavor. It is smoky, for sure, but also has a very delicate, fresh taste, topped with a tangy citrus drizzle." _
         & vbCrLf & vbCrLf _
         & "Served with a side of herb roasted red potatoes and your choice of broccoli, brussel sprouts, or asparagus!"
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("Christopher's Hole in the Wall cafe is the homade dining experience you've been waiting for! Press a button below to see our daily specials!")
     End Sub
 End Class
